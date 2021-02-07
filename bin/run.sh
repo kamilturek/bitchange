03b45bf6de3a0febdf26efbe56a2e48ea504b947
Kamil Turek <kamil.turek@hotmail.com>
2021-02-07 23:00:24 +0100

    Fix shellcheck errors

--- bin/run.sh
+++ bin/run.sh
@@ -4,16 +4,15 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-ROOT_DIR=$(dirname $(dirname $(realpath $0)))
+ROOT_DIR="$(dirname "$(dirname "$(realpath "$0")")")"
 
 
 case "$1" in
-
     manage)
         docker-compose run --rm web python manage.py "${@:2}"
     ;;
     test)
-        docker-compose -f $ROOT_DIR/docker-compose-test.yml run --rm test_web "${@:2}"
+        docker-compose -f "$ROOT_DIR"/docker-compose-test.yml run --rm test_web "${@:2}"
     ;;
     *)
         echo "ERROR: Unknown operation."  
