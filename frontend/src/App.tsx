62ee132de54b4d31e3f3413cf47cdef8f6a117a6
Kamil Turek <kamil.turek@hotmail.com>
2021-02-13 12:50:18 +0100

    Setup dummy routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 function App() {
-  return <div>testest</div>;
+  return (
+    <Router>
+      <Link to='/'>Home</Link>
+      <Link to='/login'>Login</Link>
+      <Link to='/register'>Register</Link>
+
+      <Switch>
+        <Route path='/' exact={true}>Home Page</Route>
+        <Route path='/login'>Login Page</Route>
+        <Route path='/register'>Register Page</Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
