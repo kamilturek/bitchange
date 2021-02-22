c72c9dd86437c78e867dcae69ab972c4b64f83f8
Kamil Turek <kamil.turek@hotmail.com>
2021-02-22 22:42:35 +0100

    Sort imports one more time

--- backend/bitchange/users/models.py
+++ backend/bitchange/users/models.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.models import UserManager as BaseUserManager, AbstractUser
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import UserManager as BaseUserManager
 
 
 class UserManager(BaseUserManager):
